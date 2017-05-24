28c91cc9918c96a95ba9bd74e7e6632cd799d8c6
Daniel Endres <danielendres@gmail.com>
2017-05-24 08:28:38 -0700

    add bootstrap and add coordinates to destinations

--- Gemfile
+++ Gemfile
@@ -10,6 +10,8 @@ gem 'figaro'
 
 gem 'jquery-rails'
 
+gem 'bootstrap-sass'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.1'
 # Use sqlite3 as the database for Active Record
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170523153351) do
+ActiveRecord::Schema.define(version: 20170524151706) do
 
   create_table "destinations", force: :cascade do |t|
     t.string "city"
     t.string "country"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
   end
 
 end
