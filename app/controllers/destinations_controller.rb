6e91f3ad749ba00fa98801f53461225760693174
Daniel Endres <danielendres@gmail.com>
2017-05-24 08:53:52 -0700

    destinatios view

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -64,7 +64,7 @@ class DestinationsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_destination
-      @destination = Destination.find(params[:id])
+     #  @destination = Destination.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'welcome#index'
 
+  get 'destination_show' => 'destinations#show' 
+
   resources :destinations
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
